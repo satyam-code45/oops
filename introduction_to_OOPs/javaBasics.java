04f79e45160de85f006d4734271d760d290f72b4
satyam-code45 <satyam45.dev@gmail.com>
2025-07-28 07:43:36 +0530

    numberdata types

---
+++ introduction_to_OOPs/javaBasics.java
@@ -0,0 +1,16 @@
+
+class javaBasics {
+
+    public static void main(String[] args) {
+        //the following line prints
+        System.out.print("Hello! Satyam \n");
+        int num = 45;
+        float x = 4;
+        double y = 4.555354344442332;
+        System.out.print(num );
+        System.out.print("\n");
+        System.out.print(x);
+        System.out.print("\n");
+        System.out.print(y);
+    }
+}
