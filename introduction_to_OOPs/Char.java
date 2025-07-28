b29ca6a0c489764631ff5f29c0f4ab9f821a7865
satyam-code45 <satyam45.dev@gmail.com>
2025-07-28 07:51:23 +0530

    char and boolaen

---
+++ introduction_to_OOPs/Char.java
@@ -0,0 +1,10 @@
+class Char {
+    public static void main (String[] args ){
+        char ch = 'a';
+        System.out.println(ch);
+        System.out.print("\n");
+
+        boolean t = true;
+        System.out.print(t);
+    }
+}
