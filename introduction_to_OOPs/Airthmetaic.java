1a1a7e1123958c0b21f81e134df2fb0d618acf9a
satyam-code45 <satyam45.dev@gmail.com>
2025-07-28 19:31:54 +0530

    airthematic

---
+++ introduction_to_OOPs/Airthmetaic.java
@@ -0,0 +1,19 @@
+class Airthmetaic {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 4;
+        int mul = num1 * num2;
+        int div = num1 / num2;
+        int mod = num1 % num2;
+        System.out.print("Num1: " + num1);
+        System.out.print("\n");
+        System.out.print("Num2: " + num2);
+        System.out.print("\n");
+        System.out.print(num1 + " * " + num2 + " = " + mul);
+        System.out.print("\n");
+        System.out.print(num1 + " / " + num2 + " = " + div);
+        System.out.print("\n");
+        System.out.print(num1 + " % " + num2 + " = " + mod);
+        System.out.print("\n");
+    }    
+}
